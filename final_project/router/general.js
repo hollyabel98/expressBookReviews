43d135c37956412e041dfecb084e175abb793cc3
Holly <hollyabel@hotmail.co.uk>
2026-05-03 06:45:19 -0400

    “title”

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -45,8 +45,22 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title= req.params.title;
+    const booksByTitle = [];
+
+
+        for (let isbn in books) {
+        if (books[isbn].title === title) {
+            booksByTitle.push({ isbn: isbn, ...books[isbn] });
+        }
+    }
+
+
+    if (booksByTitle.length > 0) {
+        res.send(booksByTitle);
+    } else {
+        res.status(404).json({ message: "No books found with this title" });
+    }
 });
 
 //  Get book review
