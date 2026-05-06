e4e4f46f04c5613f49350881e0fd0fc22b1d99cd
Holly <hollyabel@hotmail.co.uk>
2026-05-06 01:30:25 -0400

    “fix”

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-    app.use("/customer/auth/*", function auth(req, res, next) {
+
         const authHeader = req.headers.authorization;
     
         if (authHeader) {
