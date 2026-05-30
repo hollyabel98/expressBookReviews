fc41cb35d80e025f0ebffaac9bf5e850d9c0a9d9
Holly <hollyabel@hotmail.co.uk>
2026-05-30 07:10:48 -0400

    “promise”

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -29,38 +29,26 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/', function (req, res) {
+public_users.get('/isbn/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+  
     new Promise((resolve, reject) => {
-      resolve(JSON.stringify(books));
+      const book = books[isbn];
+      if (book) {
+        resolve(book);
+      } else {
+        reject("Book not found");
+      }
     })
-    .then((successMessage) => {
-      console.log("From Callback " + successMessage);
-      res.send(successMessage);  
+    .then((book) => {
+      console.log("Book found:", book);
+      res.json(book);
     })
     .catch((error) => {
-      return res.status(400).json({ message: error });
+      res.status(404).json({ message: error });
     });
-  });
-  
+  });  
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn', async function (req, res) {
-    try {
-      const isbn = req.params.isbn;             // Get ISBN from URL params
-      const book = books[isbn];                  // Find the book by ISBN
-  
-      if (book) {
-        console.log("Book found: ", book);
-        res.json(book);                          // Send book details as JSON
-      } else {
-        res.status(404).json({ message: "Book not found" });
-      }
-  
-    } catch (error) {
-      res.status(400).json({ message: error.message });
-    }
-  });
-  
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
