063af456663e68ee89e888835b3a39cd9cceba24
Holly <hollyabel@hotmail.co.uk>
2026-05-31 02:23:34 -0400

    “promise”

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,7 +4,7 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-// Check if user exists 
+// Check if user already exists 
 const doesExist = (username) => {
     return users.some(user => user.username === username);
 };
@@ -15,16 +15,13 @@ public_users.post("/register", (req,res) => {
 
     // Check if username and password are provided
     if (username && password) {
-        // Check if the user does not already exist
         if (!doesExist(username)) {
-            // Add the new user to the users array
             users.push({"username": username, "password": password});
             return res.status(200).json({message: "User successfully registered. You can now login"});
         } else {
             return res.status(404).json({message: "User already exists!"});
         }
     }
-    // Return error if username or password is missing
     return res.status(404).json({message: "Unable to register user."});
 });
 
@@ -50,7 +47,7 @@ public_users.get('/isbn/:isbn', function (req, res) {
   });  
 
   
-// Get book details based on author
+// Get book based on author
 public_users.get('/author/:author', function (req, res) {
     const author = req.params.author;
   
@@ -79,7 +76,7 @@ public_users.get('/author/:author', function (req, res) {
   
 
 
-// Get all books based on title
+// Get books based on title
 public_users.get('/title/:title',function (req, res) {
     const title = req.params.title;
     const booksByTitle = [];
